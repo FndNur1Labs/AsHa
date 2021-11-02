a6725d14a4cf65acaa45c20ca16019ff5943a43b
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2021-11-02 16:06:47 +0700

    Const

--- base/src/httpserver.cpp
+++ base/src/httpserver.cpp
@@ -618,7 +618,7 @@ CService HTTPRequest::GetPeer()
         // evhttp retains ownership over returned address string
         const char* address = "";
         uint16_t port = 0;
-        evhttp_connection_get_peer(con, (char**)&address, &port);
+        evhttp_connection_get_peer(con, (const char**)&address, &port);
         peer = CService(address, port);
     }
     return peer;
